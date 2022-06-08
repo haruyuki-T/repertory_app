970bc869ff9e38de14b6fe1623eeca059463e46d
塚田治幸 <tsukadaharuyuki@tsukadaharuyukinoMacBook-Air.local>
2022-06-09 02:00:24 +0900

    name validation追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,9 +24,12 @@
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 class User < ApplicationRecord
+  validates :name, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :posts, dependent: :destroy
 end
