bc5f2c17250519f497e6f5d9dc8370e0f100c5c0
塚田治幸 <tsukadaharuyuki@tsukadaharuyukinoMacBook-Air.local>
2022-06-21 03:31:57 +0900

    検索結果を日本語で取得

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -2,6 +2,7 @@
 
 class MusicsController < ApplicationController
   require "rspotify"
+  ENV['ACCEPT_LANGUAGE'] = "ja"
   RSpotify.authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_SECRET_ID"])
 
   def search
