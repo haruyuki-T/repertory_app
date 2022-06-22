841c5f269bf9dcc9c598526c2092bdd4aad9118c
塚田治幸 <tsukadaharuyuki@tsukadaharuyukinoMacBook-Air.local>
2022-06-22 23:23:44 +0900

    seed導入

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+scenes = ["未分類","同年代","上司"]
+scenes.each do |scene|
+  Scene.create(title: scene)
+end
