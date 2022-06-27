ec7a7698840693e19b91976236b3e1e75d79370f
塚田治幸 <tsukadaharuyuki@tsukadaharuyukinoMacBook-Air.local>
2022-06-27 22:42:12 +0900

    factoryと曲レパートリー追加のsystem spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,27 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  name                   :string(255)      not null
+#  email                  :string(255)      not null
+#  sex                    :integer
+#  age                    :integer
+#  introduction           :text(65535)
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
 FactoryBot.define do
   factory :user do
     name { "Taro" }
