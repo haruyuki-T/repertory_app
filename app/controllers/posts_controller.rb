7eeaeb6d3005807a408c04db04837405ac0038dc
塚田治幸 <tsukadaharuyuki@tsukadaharuyukinoMacBook-Air.local>
2022-06-27 22:52:00 +0900

    編集,削除アクションとview作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,8 +21,34 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @posts = Post.where(user_id: current_user.id)
+    @default_scene = Post.where(scene_id: 1).order(:artist_name)
+    @same_age_scene = Post.where(scene_id: 2).order(:artist_name)
+    @boss_scene = Post.where(scene_id: 3).order(:artist_name)
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.user_id == current_user.id
+      @post.update(scene_params)
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    flash[:notice] = "曲を削除しました。"
+    redirect_back(fallback_location: root_path)
+  end
+
   private
     def post_params
       params.permit(:user_id, :scene_id, :artist_name, :song_title)
+    end
+
+    def scene_params
+      params.require(:post).permit(:scene_id)
     end
 end
