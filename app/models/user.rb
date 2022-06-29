79fde03e685617bba9c5289635441d5c7c3a7b2b
塚田治幸 <tsukadaharuyuki@tsukadaharuyukinoMacBook-Air.local>
2022-06-30 03:47:46 +0900

    rubocop自動修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,7 +33,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
 
   def self.guest
-    find_or_create_by!(email: 'guest@example.com') do |user|
+    find_or_create_by!(email: "guest@example.com") do |user|
       user.password = SecureRandom.urlsafe_base64
       user.name = "guest"
     end
